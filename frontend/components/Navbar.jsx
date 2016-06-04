17d81f421ba2f5c0f6dfeb19d81b4eaf7d506fe3
Stefan Le Noach <stefanlenoach@gmail.com>
2016-06-04 15:17:02 -0400

    search bar styled, works except for submit

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -17,6 +17,10 @@ module.exports = React.createClass({
     this.setState({ searchString: e.target.value });
   },
 
+  showUser: function(){
+
+  },
+
   handleSubmit: function(e){
 
   },
@@ -31,29 +35,35 @@ module.exports = React.createClass({
         });
     }
     return (
-      <nav className='navbar'>
-        <nav className='mainnav'>
-        </nav>
-          <button className='logout-btn' onClick={ SessionApiUtil.logout }>Log out</button>
+      <div>
+        <nav className='navbar'>
+          <nav className='mainnav'>
+          </nav>
+            <button className='logout-btn' onClick={ SessionApiUtil.logout }>Log out</button>
 
-          <form className='search' onSubmit={this.handleSubmit}>
-            <input className='searchbar' type='text' onChange={this.handleChange}
-            placeholder='Search for people, jobs, companies and more...'/>
+            <form className='search' onSubmit={this.handleSubmit}>
+              <input className='searchbar' type='text' onChange={this.handleChange}
+              placeholder='Search for people, jobs, companies and more...'/>
 
-            <input className='search-btn' type='submit' value='Search'/>
-          </form>
-          <ul>
-              { users.map(function(user){
-                  return (<li>{user.first_name + " " + user.last_name} <a href={user.url}>{user.url}</a></li>);
-                })
-              }
+              <input className='search-btn' type='submit' value='Search'/>
+            </form>
+
+          <nav className='subnav'></nav>
+            <Link className='nav-link' to='/home'>Home</Link>
+            <Link className='nav-link' to='/profile'>Profile</Link>
+            <Link className='nav-link' to='/connections'>Connections</Link>
+            <nav>
+            <ul className = 'search-items'>
+            { users.map(function(user){
+              return (<li className='search-item'>
+              <a href={this.showUser}>{user.first_name + " " + user.last_name}</a>
+              </li>);
+            })
+          }
           </ul>
-
-        <nav className='subnav'></nav>
-          <Link className='nav-link' to='/home'>Home</Link>
-          <Link className='nav-link' to='/profile'>Profile</Link>
-          <Link className='nav-link' to='/connections'>Connections</Link>
-      </nav>
+          </nav>
+        </nav>
+    </div>
     );
   }
 });
